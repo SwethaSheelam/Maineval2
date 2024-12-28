fdb81f185e9ba414910be9d6e1e4c0fbff2074ba
sheelamswetha <sheelamswetha5@gmail.com>
2024-12-28 15:54:04 +0530

    admin page form credentials verified

---
+++ scripts/admin.js
@@ -0,0 +1,19 @@
+let form=document.getElementById("form");
+form.addEventListener("submit",function(){
+   
+
+    let bookselected=form.book.value;
+    let author=form.author.value;
+    let category=form.category.value;
+    let correctbook=form.select.value;
+    //console.log(bookselected,book1,book2)
+    let selectedobj=
+    {
+        title,
+        author,
+        category,
+        isAvailable,
+        isVerified,
+        "imageUrl": "https://m.media-amazon.com/images/I/71ZB18P3inL._SY522_.jpg"
+      }
+})
